bbc92eeb75b375442a03971cfc8c268bb44ce5aa
Dave1001 <dave@walnut-street.net>
2018-03-26 23:52:40 +1000

    Updated build.sh
    Added an option to clear flash

--- build.sh
+++ build.sh
@@ -35,14 +35,14 @@ else
     echo "Debug mode is off"
 fi
 # --------------------
-# Set the SDK path
-export SDK_PATH="$HOME/esp-open-rtos"
-echo "SDK path set to $SDK_PATH"
+# Check whether to erase firmware (this value is used later on)
+echo "Erasing the firmware is recommended, however will also reset the accessory"
+echo "serial number and other parameters. You will need to re-pair with your Homekit app"
+echo "Do you want to erase previous firmware? [Y or N] : "
+read -t5 -n1 -r ERASE_FIRMWARE_VAL
+: "${ERASE_FIRMWARE_VAL:=N}"
 # --------------------
-# From this point exit the script on error
-set -e
-# --------------------
-# Start the build
+# Check whether  build parameters are OK
 if [ -z "$1" ] || [ -z "$2" ]
 then
     echo "Nothing to build or incorrect parameters"
@@ -50,17 +50,32 @@ then
     echo "For example \"./build.sh examples/led /dev/ttyUSB0\""
     exit 1
 fi
-echo "Commencing build of $1..."
-make -C $1 all
 # --------------------
-# Upload to flash
+# Set the SDK path
+export SDK_PATH="$HOME/esp-open-rtos"
+echo "SDK path set to $SDK_PATH"
+# --------------------
+# From this point exit the script on error
+set -e
+# --------------------
+# Set the serial port
 export ESPPORT="$2"
 # The next line is a workaround - refer to:
 # https://askubuntu.com/questions/210177/serial-port-terminal-cannot-open-dev-ttys0-permission-denied
 sudo chmod 666 "$2"
 echo "ESP port set to $ESPPORT"
-echo "Erasing previous firmware..."
-make -C $1 erase_flash
+# --------------------
+# Commence the build!
+echo "Commencing build of $1..."
+make -C $1 all
+# --------------------
+# Erase flash if needed
+if [ $ERASE_FIRMWARE_VAL = "Y" ] || [ $ERASE_FIRMWARE_VAL = "y" ]
+then
+    make -C $1 erase_flash
+fi
+# --------------------
+# Upload to flash
 echo "Uploading firmware to flash..."
 make -C $1 flash
 echo "All done. Press a key within 10 sec to monitor, or run \"make -C $1 monitor\"..."
