d979a92ca34dd8333ba60c7f004d9d7aedfca486
Paulo Cesar Saito Lopes <paulostool@gmail.com>
2018-05-03 18:06:53 -0300

    implements a low pass filter on gpio value instead of a time debounce to toggle
    fix

--- examples/sonoff_basic_toggle/toggle.c
+++ examples/sonoff_basic_toggle/toggle.c
@@ -2,15 +2,20 @@
 #include <esplibs/libmain.h>
 #include "toggle.h"
 
+#define LPF_SHIFT 3  // divide by 8
+#define LPF_INTERVAL 10  // in milliseconds
+
+#define maxvalue_unsigned(x) ((1<<(8*sizeof(x)))-1)
 
 typedef struct _toggle {
     uint8_t gpio_num;
     toggle_callback_fn callback;
 
-    uint16_t debounce_time;
     uint8_t state;
+    uint16_t value;
     uint32_t last_event_time;
 
+    TaskHandle_t task_handle;
     struct _toggle *next;
 } toggle_t;
 
@@ -26,31 +31,28 @@ static toggle_t *toggle_find_by_gpio(const uint8_t gpio_num) {
     return toggle;
 }
 
-
-
-
-
-void toggle_intr_callback(uint8_t gpio) {
+void toggleService(void *_args) {
+    const TickType_t xPeriod = pdMS_TO_TICKS(LPF_INTERVAL);
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    
+    uint8_t gpio = (uint8_t)((uint32_t)_args);
     toggle_t *toggle = toggle_find_by_gpio(gpio);
     if (!toggle)
-        return;
+        vTaskDelete(NULL);
 
-    uint32_t now = xTaskGetTickCountFromISR();
-    if ((now - toggle->last_event_time)*portTICK_PERIOD_MS < toggle->debounce_time) {
-        // debounce time, ignore events
-        return;
-    }
-    toggle->last_event_time = now;
-    if (gpio_read(toggle->gpio_num) != toggle->state) {
-        // different state = toggled
-        toggle->state = gpio_read(toggle->gpio_num);
-        toggle->callback(toggle->gpio_num);
+    for (;;) {
+        toggle->value += ((gpio_read(toggle->gpio_num) * maxvalue_unsigned(toggle->value)) - toggle->value) >> LPF_SHIFT ;
+        uint8_t state = (toggle->value > (maxvalue_unsigned(toggle->value) / 2));
+
+        if (state != toggle->state) {
+            // different state = toggled
+            toggle->state = state;
+            toggle->callback(toggle->gpio_num);
+        }
+
+        vTaskDelayUntil(&xLastWakeTime, xPeriod);
     }
 }
-
-
-
-
 
 int toggle_create(const uint8_t gpio_num, toggle_callback_fn callback) {
     toggle_t *toggle = toggle_find_by_gpio(gpio_num);
@@ -62,9 +64,6 @@ int toggle_create(const uint8_t gpio_num, toggle_callback_fn callback) {
     toggle->gpio_num = gpio_num;
     toggle->callback = callback;
 
-    // times in milliseconds
-    toggle->debounce_time = 50;
-    
     // initial state is as initilised
     toggle->state = gpio_read(gpio_num);
 
@@ -75,11 +74,15 @@ int toggle_create(const uint8_t gpio_num, toggle_callback_fn callback) {
     toggles = toggle;
 
     gpio_set_pullup(toggle->gpio_num, true, true);
-    gpio_set_interrupt(toggle->gpio_num, GPIO_INTTYPE_EDGE_ANY, toggle_intr_callback);
+
+    char service_name[16];
+    snprintf(service_name, sizeof service_name, "%s%d", "toggleService", gpio_num);
+    BaseType_t task_created = xTaskCreate(toggleService, service_name, 255, (void *)((uint32_t)gpio_num), 2, &toggle->task_handle);
+    if (task_created != pdPASS)
+        return -1;
 
     return 0;
 }
-
 
 void toggle_delete(const uint8_t gpio_num) {
     if (!toggles)
@@ -100,8 +103,6 @@ void toggle_delete(const uint8_t gpio_num) {
         }
     }
 
-    if (toggle) {
-        gpio_set_interrupt(gpio_num, GPIO_INTTYPE_EDGE_ANY, NULL);
-    }
+    if (toggle)
+        vTaskDelete(toggle->task_handle);
 }
-
