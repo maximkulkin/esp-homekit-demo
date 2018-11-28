8dfba836fd22340539b1ec0925ce83796693bd43
Dirk T <d.theisen@objectpark.org>
2018-11-28 01:11:40 +0000

    Added relay_actor type / files.

---
+++ examples/garage/relay_actor.h
@@ -0,0 +1,14 @@
+//
+//  relay_actor.h
+//  garage
+//
+//  Created by Dirk Theisen on 24.11.18.
+//  Copyright © 2018 Dirk Theisen. All rights reserved.
+//
+
+#ifndef relay_actor_h
+#define relay_actor_h
+
+#include <stdio.h>
+
+#endif /* relay_actor_h */
