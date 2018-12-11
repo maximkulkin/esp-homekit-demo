f52b7e6e671e8ca98f95769f99a5ffe2ce38ca06
idum_zaes <idum_zaes@me.com>
2018-12-11 16:31:04 -0500

    Workbench lightstrip with Wifi_Config. Using GPIO 2 for lightstrip pin. Build Successfull.

---
+++ examples/workbench/workbench.c
@@ -0,0 +1,121 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <espressif/esp_common.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include <wifi_config.h>
+
+
+const int led_gpio = 2;
+bool led_on = false;
+
+void led_write(bool on) {
+    gpio_write(led_gpio, on ? 0 : 1);
+}
+
+void led_init() {
+    gpio_enable(led_gpio, GPIO_OUTPUT);
+    led_write(led_on);
+}
+
+void led_identify_task(void *_args) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            led_write(true);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            led_write(false);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    led_write(led_on);
+
+    vTaskDelete(NULL);
+}
+
+void led_identify(homekit_value_t _value) {
+    printf("LED identify\n");
+    xTaskCreate(led_identify_task, "LED identify", 128, NULL, 2, NULL);
+}
+
+homekit_value_t led_on_get() {
+    return HOMEKIT_BOOL(led_on);
+}
+
+void led_on_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    led_on = value.bool_value;
+    led_write(led_on);
+}
+
+homekit_characteristic_t ne = HOMEKIT_CHARACTERISTIC_(MANUFACTURER, NULL);
+
+static void get_ip(void){
+  static char ip[16];
+  ip[0] = 0;
+  struct ip_info ipinfo;
+  (void)
+  sdk_wifi_get_ip_info(STATION_IF, &ipinfo);
+  snprintf(ip, sizeof(ip), IPSTR, IP2STR(&ipinfo.ip));
+  ne.value.string_value = strdup(ip);
+}
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            &ne,
+            HOMEKIT_CHARACTERISTIC(NAME, "Workbench"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "zaes"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "037A2BABF19D"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "MyLED"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
+            HOMEKIT_CHARACTERISTIC(
+                ON, false,
+                .getter=led_on_get,
+                .setter=led_on_set
+            ),
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+  .accessories = accessories,
+  .password = "718-92-671"
+};
+
+void on_wifi_ready() {
+  get_ip();
+  homekit_server_init(&config);
+}
+
+void user_init(void) {
+    uart_set_baud(0, 115200);
+
+    wifi_config_init("Workbench-", NULL, on_wifi_ready);
+    led_init();
+    homekit_server_init(&config);
+}
