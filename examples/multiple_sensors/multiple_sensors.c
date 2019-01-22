e85792cdd8ccf8a7aeded77858bd1c3a03c7feea
sakisdog <sakisdog+github@gmail.com>
2019-01-22 23:27:13 +0200

    Update multiple_sensors.c

--- examples/multiple_sensors/multiple_sensors.c
+++ examples/multiple_sensors/multiple_sensors.c
@@ -20,6 +20,7 @@
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 #include <wifi_config.h>
+#include <dht/dht.h>
 
 homekit_characteristic_t name             = HOMEKIT_CHARACTERISTIC_(NAME, DEVICE_NAME);
 homekit_characteristic_t manufacturer     = HOMEKIT_CHARACTERISTIC_(MANUFACTURER,  DEVICE_MANUFACTURER);
@@ -27,6 +28,12 @@ homekit_characteristic_t serial           = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBE
 homekit_characteristic_t model            = HOMEKIT_CHARACTERISTIC_(MODEL,         DEVICE_MODEL);
 homekit_characteristic_t revision         = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION,  FW_VERSION);
 homekit_characteristic_t motion_detected  = HOMEKIT_CHARACTERISTIC_(MOTION_DETECTED, 0);
+homekit_characteristic_t temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+homekit_characteristic_t humidity    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+#ifndef SENSOR_PIN
+#error SENSOR_PIN is not specified
+#endif
 
 void led_write(bool on) {
     gpio_write(LED_GPIO, on ? 0 : 1);
@@ -52,6 +59,35 @@ void identify(homekit_value_t _value) {
     printf("identify\n\n");
     xTaskCreate(identify_task, "identify", 128, NULL, 2, NULL);
 }
+void temperature_sensor_identify(homekit_value_t _value) {
+    printf("Temperature sensor identify\n");
+}
+
+void temperature_sensor_task(void *_args) {
+    gpio_set_pullup(SENSOR_PIN, false, false);
+
+    float humidity_value, temperature_value;
+    while (1) {
+        bool success = dht_read_float_data(
+            DHT_TYPE_DHT22, SENSOR_PIN,
+            &humidity_value, &temperature_value
+        );
+        if (success) {
+            temperature.value.float_value = temperature_value;
+            humidity.value.float_value = humidity_value;
+
+            homekit_characteristic_notify(&temperature, HOMEKIT_FLOAT(temperature_value));
+            homekit_characteristic_notify(&humidity, HOMEKIT_FLOAT(humidity_value));
+        } else {
+            printf("Couldnt read data from sensor\n");
+        }
+
+        vTaskDelay(6000 / portTICK_PERIOD_MS);
+    }
+}
+void temperature_sensor_init() {
+    xTaskCreate(temperature_sensor_task, "Temperature Sensor", 256, NULL, 2, NULL);
+}
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]){
@@ -67,6 +103,28 @@ homekit_accessory_t *accessories[] = {
         HOMEKIT_SERVICE(MOTION_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
             HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
             &motion_detected,
+            NULL
+        }),
+        NULL
+    }),
+    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_thermostat, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            &name,
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "YP"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0012345"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "MultipleSensors"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, temperature_sensor_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            &temperature,
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &humidity,
             NULL
         }),
         NULL
@@ -101,4 +159,5 @@ void user_init(void) {
     create_accessory_name();
     wifi_config_init("motion-sensor", NULL, on_wifi_ready);
     gpio_init();
+    temperature_sensor_init();
 }
