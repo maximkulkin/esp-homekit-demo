0c748802527d4794c0244178a0154d57a89feeb3
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-12-17 11:21:38 -0800

    Refactor garage example

--- examples/garage/garage.c
+++ examples/garage/garage.c
@@ -14,6 +14,7 @@
 #include "wifi.h"
 #include "contact_sensor.h"
 
+
 // Possible values for characteristic CURRENT_DOOR_STATE:
 #define HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPEN 0
 #define HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED 1
@@ -28,18 +29,6 @@
 
 #define OPEN_CLOSE_DURATION 22
 
-const char *state_description(uint8_t state) {
-    const char* description = "unknown";
-    switch (state) {
-        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPEN: description = "open"; break;
-        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING: description = "opening"; break;
-        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED: description = "closed"; break;
-        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING: description = "closing"; break;
-        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_STOPPED: description = "stopped"; break;
-        default: ;
-    }
-    return description;
-}
 
 static void wifi_init() {
     struct sdk_station_config wifi_config = {
@@ -52,15 +41,32 @@ static void wifi_init() {
     sdk_wifi_station_connect();
 }
 
+
+const char *state_description(uint8_t state) {
+    switch (state) {
+        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPEN: return "open";
+        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING: return "opening";
+        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED: return "closed";
+        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING: return "closing";
+        case HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_STOPPED: return "stopped";
+        default: return "unknown";
+    }
+}
+
+
 // Declare functions:
 
-homekit_value_t gdo_target_state_get();
-void gdo_target_state_set(homekit_value_t new_value);
+void gdo_target_state_changed(homekit_characteristic_t *ch, homekit_value_t value, void *context);
 homekit_value_t gdo_current_state_get();
-homekit_value_t gdo_obstruction_get();
 void identify(homekit_value_t _value);
 
 // Declare global variables:
+
+homekit_characteristic_t ch_current_door_state = HOMEKIT_CHARACTERISTIC_(
+    CURRENT_DOOR_STATE, HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED,
+    .getter=gdo_current_state_get,
+);
+
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_garage, .services=(homekit_service_t*[]){
@@ -75,21 +81,12 @@ homekit_accessory_t *accessories[] = {
         }),
         HOMEKIT_SERVICE(GARAGE_DOOR_OPENER, .primary=true, .characteristics=(homekit_characteristic_t*[]){
             HOMEKIT_CHARACTERISTIC(NAME, "Tor"),
-            HOMEKIT_CHARACTERISTIC(
-                CURRENT_DOOR_STATE, HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED,
-                .getter=gdo_current_state_get,
-                .setter=NULL
-            ),
+            &ch_current_door_state,
             HOMEKIT_CHARACTERISTIC(
                 TARGET_DOOR_STATE, HOMEKIT_CHARACTERISTIC_TARGET_DOOR_STATE_CLOSED,
-                .getter=gdo_target_state_get,
-                .setter=gdo_target_state_set
+                .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(gdo_target_state_changed),
             ),
-            HOMEKIT_CHARACTERISTIC(
-                OBSTRUCTION_DETECTED, HOMEKIT_CHARACTERISTIC_TARGET_DOOR_STATE_CLOSED,
-                .getter=gdo_obstruction_get,
-                .setter=NULL
-            ),
+            HOMEKIT_CHARACTERISTIC(OBSTRUCTION_DETECTED, false),
             NULL
         }),
         NULL
@@ -99,7 +96,7 @@ homekit_accessory_t *accessories[] = {
 
 bool relay_on = false;
 uint8_t current_door_state = HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_UNKNOWN;
-ETSTimer update_timer; // used for delayed updating from contact sensor
+ETSTimer door_runaway_timer; // used for delayed updating from contact sensor
 
 
 
@@ -107,10 +104,12 @@ void relay_write(bool on) {
     gpio_write(RELAY_PIN, on ? 0 : 1);
 }
 
+
 void relay_init() {
     gpio_enable(RELAY_PIN, GPIO_OUTPUT);
     relay_write(relay_on);
 }
+
 
 void identify_task(void *_args) {
     // 1. move the door, 2. stop it, 3. move it back:
@@ -126,68 +125,23 @@ void identify_task(void *_args) {
     vTaskDelete(NULL);
 }
 
+
 void identify(homekit_value_t _value) {
     printf("GDO identify\n");
     xTaskCreate(identify_task, "GDO identify", 128, NULL, 2, NULL);
 }
 
-homekit_value_t relay_on_get() {
-    return HOMEKIT_BOOL(relay_on);
-}
-
-void relay_on_set(homekit_value_t value) {
-    if (value.format != homekit_format_bool) {
-        printf("Invalid value format: %d\n", value.format);
-        return;
-    }
-
-    relay_on = value.bool_value;
-    relay_write(relay_on);
-}
-
-homekit_value_t gdo_obstruction_get() {
-    return HOMEKIT_BOOL(false);
-}
-
-void gdo_current_state_notify_homekit() {
-
-    homekit_value_t new_value = HOMEKIT_UINT8(current_door_state);
-    printf("Notifying homekit that current door state is now '%s'\n", state_description(current_door_state));
-
-    // Find the current door state characteristic c:
-    homekit_accessory_t *accessory = accessories[0];
-    homekit_service_t *service = accessory->services[1];
-    homekit_characteristic_t *c = service->characteristics[1];
-
-    assert(c);
-
-    printf("Notifying changed '%s'\n", c->description);
-    homekit_characteristic_notify(c, new_value);
-}
-
-void gdo_target_state_notify_homekit() {
-
-    homekit_value_t new_value = gdo_target_state_get();
-    printf("Notifying homekit that target door state is now '%s'\n", state_description(new_value.int_value));
-
-    // Find the target door state characteristic c:
-    homekit_accessory_t *accessory = accessories[0];
-    homekit_service_t *service = accessory->services[1];
-    homekit_characteristic_t *c = service->characteristics[2];
-
-    assert(c);
-
-    printf("Notifying changed '%s'\n", c->description);
-    homekit_characteristic_notify(c, new_value);
-}
 
 void current_state_set(uint8_t new_state) {
     if (current_door_state != new_state) {
         current_door_state = new_state;
-        gdo_target_state_notify_homekit();
-        gdo_current_state_notify_homekit();
+
+        homekit_characteristic_notify(
+            &ch_current_door_state, HOMEKIT_UINT8(current_door_state)
+        );
     }
 }
+
 
 void current_door_state_update_from_sensor() {
     contact_sensor_state_t sensor_state = contact_sensor_state_get(REED_PIN);
@@ -204,60 +158,31 @@ void current_door_state_update_from_sensor() {
     }
 }
 
+
 homekit_value_t gdo_current_state_get() {
     if (current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_UNKNOWN) {
-	current_door_state_update_from_sensor();
+        current_door_state_update_from_sensor();
     }
-    printf("returning current door state '%s'.\n", state_description(current_door_state));
+
+    printf("returning current door state '%s'.\n",
+           state_description(current_door_state));
 
     return HOMEKIT_UINT8(current_door_state);
 }
 
-homekit_value_t gdo_target_state_get() {
-    uint8_t result = gdo_current_state_get().int_value;
-    if (result == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING) {
-	result = HOMEKIT_CHARACTERISTIC_TARGET_DOOR_STATE_OPEN;
-    } else if (result == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING) {
-        result = HOMEKIT_CHARACTERISTIC_TARGET_DOOR_STATE_CLOSED;  
-    }
 
-    printf("Returning target door state '%s'.\n", state_description(result));
-
-    return HOMEKIT_UINT8(result);
-}
-
-/**
- * Called (indirectly) from the interrupt handler to notify the client of a state change.
- **/
-void contact_sensor_state_changed(uint8_t gpio, contact_sensor_state_t state) {
-
-    printf("contact sensor state '%s'.\n", state == CONTACT_OPEN ? "open" : "closed");
-
-    if (current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING ||
-        current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING) {
-	// Ignore the event - the state will be updated after the time expired!
-        printf("contact_sensor_state_changed() ignored during opening or closing.\n");
-	return;
-    }
-    current_door_state_update_from_sensor();
-}
-
-
-void gdo_target_state_set(homekit_value_t new_value) {
-
-    if (new_value.format != homekit_format_uint8) {
-        printf("Invalid value format: %d\n", new_value.format);
+void gdo_target_state_changed(homekit_characteristic_t *ch, homekit_value_t new_value, void *context) {
+    if (current_door_state != HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPEN &&
+        current_door_state != HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED)
+    {
+        printf("gdo_target_state_set() ignored: current state not open or closed (%s).\n",
+               state_description(current_door_state));
         return;
     }
 
-    if (current_door_state != HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPEN &&
-        current_door_state != HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED) {
-        printf("gdo_target_state_set() ignored: current state not open or closed (%s).\n", state_description(current_door_state));
-	return;
-    }
-
     if (current_door_state == new_value.int_value) {
-        printf("gdo_target_state_set() ignored: new target state == current state (%s)\n", state_description(current_door_state));
+        printf("gdo_target_state_set() ignored: new target state == current state (%s)\n",
+               state_description(current_door_state));
         return;
     }
 
@@ -268,21 +193,38 @@ void gdo_target_state_set(homekit_value_t new_value) {
     vTaskDelay(400 / portTICK_PERIOD_MS);
     // Turn OFF GPIO:
     relay_write(false);
-    if (current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED) {
-        current_state_set(HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING);
-    } else {
-        current_state_set(HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING);
-    }
+
+    current_state_set(
+        (current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSED)
+            ? HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING
+            : HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING
+    );
+
     // Wait for the garage door to open / close,
     // then update current_door_state from sensor:
-    sdk_os_timer_arm(&update_timer, OPEN_CLOSE_DURATION * 1000, false);
+    sdk_os_timer_arm(&door_runaway_timer, OPEN_CLOSE_DURATION * 1000, false);
 }
 
 
-static void timer_callback(void *arg) {
+/**
+ * Called (indirectly) from the interrupt handler to notify the client of a state change.
+ **/
+void IRAM contact_sensor_state_changed(uint8_t gpio, contact_sensor_state_t state) {
+    printf("contact sensor state '%s'.\n", state == CONTACT_OPEN ? "open" : "closed");
 
-    printf("Timer fired. Updating state from sensor.\n");
-    sdk_os_timer_disarm(&update_timer);
+    if (current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_OPENING ||
+        current_door_state == HOMEKIT_CHARACTERISTIC_CURRENT_DOOR_STATE_CLOSING) {
+        // Ignore the event - the state will be updated after the time expired!
+        printf("contact_sensor_state_changed() ignored during opening or closing.\n");
+        return;
+    }
+
+    current_door_state_update_from_sensor();
+}
+
+
+static void door_runaway_callback(void *arg) {
+    sdk_os_timer_disarm(&door_runaway_timer);
     current_door_state_update_from_sensor();
 }
 
@@ -292,6 +234,7 @@ homekit_server_config_t config = {
     .password = "111-11-111"
 };
 
+
 void user_init(void) {
     uart_set_baud(0, 9600);
 
@@ -299,9 +242,8 @@ void user_init(void) {
     relay_init();
 
     // Initialize Timer:
-    sdk_os_timer_disarm(&update_timer);
-    sdk_os_timer_setfn(&update_timer, timer_callback, NULL);
-
+    sdk_os_timer_disarm(&door_runaway_timer);
+    sdk_os_timer_setfn(&door_runaway_timer, door_runaway_callback, NULL);
 
     printf("Using Sensor at GPIO%d.\n", REED_PIN);
     if (contact_sensor_create(REED_PIN, contact_sensor_state_changed)) {
@@ -309,5 +251,4 @@ void user_init(void) {
     }
 
     homekit_server_init(&config);
-
 }
