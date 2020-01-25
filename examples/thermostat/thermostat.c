d7ad8e4eebd6b9225b57d0ef3928b1e32e974efb
Sakis <sakisdog@gmail.com>
2020-01-25 16:42:16 +0000

    Thermostat oled with buttons ESP-WROOM-02

--- examples/thermostat/thermostat.c
+++ examples/thermostat/thermostat.c
@@ -1,25 +1,15 @@
 /*
  * Thermostat example
- *
- * Wiring is as follows (requires ESP-12 as it needs 4 GPIOs):
- *
- * DHT11 (temperature sensor)
- *
- *               -------------
- *              |GND       VCC|    (These go to control pins of relay)
- *              |15         13| --> Heater
- *              |2          12| --> Cooler
- *              |0          14| --> Fan
- *              |5          16|
- * DHT Data <-- |4       CH_PD|
- *              |RXD       ADC|
- *              |TXD      REST|
- *               -------------
- *              |   |-| |-| | |
- *              | __| |_| |_| |
- *               -------------
- *
  */
+#define TEMPERATURE_SENSOR_PIN 0 // GPIO2 is D4 on NodeMCU
+#define HEATER_PIN 15 // GPIO15 is D8 on NodeMCU
+#define TEMPERATURE_POLL_PERIOD 10000 // Temp refresh rate to milliseconds
+#define TEMP_DIFF 0.5 // Set this for differential 
+#define INVERT_RELAY_SWITCH 0
+#define DHT_TYPE DHT_TYPE_DHT22 // If you are using DHT11 change the type to DHT_TYPE_DHT11
+#define BUTTON_UP_PIN 13
+#define BUTTON_DOWN_PIN 12
+
 #include <stdio.h>
 #include <espressif/esp_wifi.h>
 #include <espressif/esp_sta.h>
@@ -30,85 +20,12 @@
 #include <esplibs/libmain.h>
 #include <FreeRTOS.h>
 #include <task.h>
-
+#include <math.h>
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
-#include "wifi.h"
-
+#include "wifi_config.h"
+#include <button.h>
 #include <dht/dht.h>
-
-
-#define LED_PIN 2
-#define TEMPERATURE_SENSOR_PIN 4
-#define FAN_PIN 14
-#define COOLER_PIN 12
-#define HEATER_PIN 13
-#define TEMPERATURE_POLL_PERIOD 10000
-#define HEATER_FAN_DELAY 30000
-#define COOLER_FAN_DELAY 0
-
-
-static void wifi_init() {
-    struct sdk_station_config wifi_config = {
-        .ssid = WIFI_SSID,
-        .password = WIFI_PASSWORD,
-    };
-
-    sdk_wifi_set_opmode(STATION_MODE);
-    sdk_wifi_station_set_config(&wifi_config);
-    sdk_wifi_station_connect();
-}
-
-
-void thermostat_identify(homekit_value_t _value) {
-    printf("Thermostat identify\n");
-}
-
-
-
-
-ETSTimer fan_timer;
-
-
-void heaterOn() {
-    gpio_write(HEATER_PIN, false);
-}
-
-
-void heaterOff() {
-    gpio_write(HEATER_PIN, true);
-}
-
-
-void coolerOn() {
-    gpio_write(COOLER_PIN, false);
-}
-
-
-void coolerOff() {
-    gpio_write(COOLER_PIN, true);
-}
-
-
-void fan_alarm(void *arg) {
-    gpio_write(FAN_PIN, false);
-}
-
-void fanOn(uint16_t delay) {
-    if (delay > 0) {
-        sdk_os_timer_arm(&fan_timer, delay, false);
-    } else {
-        gpio_write(FAN_PIN, false);
-    }
-}
-
-
-void fanOff() {
-    sdk_os_timer_disarm(&fan_timer);
-    gpio_write(FAN_PIN, true);
-}
-
-
 void update_state();
 
 
@@ -126,7 +43,7 @@ homekit_characteristic_t target_temperature  = HOMEKIT_CHARACTERISTIC_(
 homekit_characteristic_t units = HOMEKIT_CHARACTERISTIC_(TEMPERATURE_DISPLAY_UNITS, 0);
 homekit_characteristic_t current_state = HOMEKIT_CHARACTERISTIC_(CURRENT_HEATING_COOLING_STATE, 0);
 homekit_characteristic_t target_state = HOMEKIT_CHARACTERISTIC_(
-    TARGET_HEATING_COOLING_STATE, 0, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(on_update)
+    TARGET_HEATING_COOLING_STATE, 0, .valid_values = {.count = 2, .values = (uint8_t[]) { 0, 1}}, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(on_update)
 );
 homekit_characteristic_t cooling_threshold = HOMEKIT_CHARACTERISTIC_(
     COOLING_THRESHOLD_TEMPERATURE, 25, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(on_update)
@@ -136,6 +53,88 @@ homekit_characteristic_t heating_threshold = HOMEKIT_CHARACTERISTIC_(
 );
 homekit_characteristic_t current_humidity = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
 
+// OLED Stuff Start
+#include <i2c/i2c.h>
+#include <ssd1306/ssd1306.h>
+#include <fonts/fonts.h>
+#define I2C_BUS 0
+#define I2C_SDA_PIN 5
+#define I2C_SCL_PIN 4
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+#define DEFAULT_FONT FONT_FACE_TERMINUS_6X12_ISO8859_1
+#define DEFAULT_FONT1 FONT_FACE_TERMINUS_BOLD_12X24_ISO8859_1
+#define DEFAULT_FONT2 FONT_FACE_TERMINUS_BOLD_14X28_ISO8859_1
+#define DEFAULT_FONT3 FONT_FACE_TERMINUS_BOLD_16X32_ISO8859_1
+
+static const ssd1306_t display = {
+    .protocol = SSD1306_PROTO_I2C,
+    .screen = SSD1306_SCREEN,
+    .i2c_dev.bus = I2C_BUS,
+    .i2c_dev.addr = SSD1306_I2C_ADDR_0,
+    .width = DISPLAY_WIDTH,
+    .height = DISPLAY_HEIGHT,
+};
+
+static uint8_t display_buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
+
+void display_init() {
+    printf("Starting display init...");
+    i2c_init(I2C_BUS, I2C_SCL_PIN, I2C_SDA_PIN, I2C_FREQ_400K);
+    if (ssd1306_init(&display)) {
+        printf("Failed to initialize OLED display\n");
+        return;
+    }
+    ssd1306_set_whole_display_lighting(&display, false);
+    ssd1306_set_scan_direction_fwd(&display, false);
+    ssd1306_set_segment_remapping_enabled(&display, true);
+    ssd1306_display_on(&display, true);
+}
+
+void display_temperature(float temperature, float humidity) {
+    //printf(" TEMP %g, HUM %g, TARGET: %g\n", temperature, humidity, target_temperature.value.float_value);
+
+    char str[16];
+    //float f = 123.456789;
+    //snprintf(str, sizeof(str), "%.2f", temperature);
+
+    //ssd1306_display_on(&display, true);
+
+    // Display temp
+    snprintf(str, sizeof(str), "%.1f", temperature);
+    ssd1306_fill_rectangle(&display, display_buffer, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, OLED_COLOR_BLACK);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 0, 0, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 74, 0, "C", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 60, 0, "°", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+    // Display humidity
+    snprintf(str, sizeof(str), "%.1f", humidity);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 0, 24, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 90, 24, "%", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+    // Display target temp
+    snprintf(str, sizeof(str), "Target: %.1f", target_temperature.value.float_value);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 0, 48, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 90, 48, "C", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 78, 48, "°", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+    if (ssd1306_load_frame_buffer(&display, display_buffer)) {
+        printf("Failed to load buffer for OLED display\n");
+    }
+}
+
+void thermostat_identify(homekit_value_t _value) {
+    printf("Thermostat identify\n");
+}
+
+void heaterOn() {
+    gpio_write(HEATER_PIN, INVERT_RELAY_SWITCH ? false : true);
+}
+
+
+void heaterOff() {
+    gpio_write(HEATER_PIN, INVERT_RELAY_SWITCH ? true : false);
+}
 
 void update_state() {
     uint8_t state = target_state.value.int_value;
@@ -144,64 +143,49 @@ void update_state() {
         if (current_state.value.int_value != 1) {
             current_state.value = HOMEKIT_UINT8(1);
             homekit_characteristic_notify(&current_state, current_state.value);
-
             heaterOn();
-            coolerOff();
-            fanOff();
-            fanOn(HEATER_FAN_DELAY);
+            
         }
     } else if ((state == 2 && current_temperature.value.float_value > target_temperature.value.float_value) ||
             (state == 3 && current_temperature.value.float_value > cooling_threshold.value.float_value)) {
         if (current_state.value.int_value != 2) {
             current_state.value = HOMEKIT_UINT8(2);
             homekit_characteristic_notify(&current_state, current_state.value);
-
-            coolerOn();
             heaterOff();
-            fanOff();
-            fanOn(COOLER_FAN_DELAY);
         }
     } else {
         if (current_state.value.int_value != 0) {
             current_state.value = HOMEKIT_UINT8(0);
             homekit_characteristic_notify(&current_state, current_state.value);
-
-            coolerOff();
             heaterOff();
-            fanOff();
         }
     }
 }
 
 
 void temperature_sensor_task(void *_args) {
-    sdk_os_timer_setfn(&fan_timer, fan_alarm, NULL);
 
-    gpio_set_pullup(TEMPERATURE_SENSOR_PIN, false, false);
-
-    gpio_enable(FAN_PIN, GPIO_OUTPUT);
-    gpio_enable(HEATER_PIN, GPIO_OUTPUT);
-    gpio_enable(COOLER_PIN, GPIO_OUTPUT);
-
-    fanOff();
-    heaterOff();
-    coolerOff();
-
-    float humidity_value, temperature_value;
+    
+    float humidity_value, temperature_value, temp_difference;
     while (1) {
         bool success = dht_read_float_data(
-            DHT_TYPE_DHT11, TEMPERATURE_SENSOR_PIN,
+            DHT_TYPE, TEMPERATURE_SENSOR_PIN,
             &humidity_value, &temperature_value
         );
         if (success) {
-            printf("Got readings: temperature %g, humidity %g\n", temperature_value, humidity_value);
+            //printf("Got readings: temperature %g, humidity %g\n", temperature_value, humidity_value);
             current_temperature.value = HOMEKIT_FLOAT(temperature_value);
             current_humidity.value = HOMEKIT_FLOAT(humidity_value);
 
             homekit_characteristic_notify(&current_temperature, current_temperature.value);
             homekit_characteristic_notify(&current_humidity, current_humidity.value);
-
-            update_state();
+	        display_temperature(temperature_value, humidity_value);
+            temp_difference = current_temperature.value.float_value - target_temperature.value.float_value;
+            //printf("temp difference: %.2f\n",fabs(temp_difference));
+            if (fabs(temp_difference) > TEMP_DIFF) {
+                //printf("updating state\n");
+                update_state();
+            }
         } else {
             printf("Couldnt read data from sensor\n");
         }
@@ -211,9 +195,8 @@ void temperature_sensor_task(void *_args) {
 }
 
 void thermostat_init() {
-    xTaskCreate(temperature_sensor_task, "Thermostat", 256, NULL, 2, NULL);
+    xTaskCreate(temperature_sensor_task, "Thermostat", 512, NULL, 2, NULL);
 }
-
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_thermostat, .services=(homekit_service_t*[]) {
@@ -221,7 +204,7 @@ homekit_accessory_t *accessories[] = {
             HOMEKIT_CHARACTERISTIC(NAME, "Thermostat"),
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
             HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "001"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "MyThermostat"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "SmartThermostat"),
             HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, thermostat_identify),
             NULL
@@ -247,12 +230,61 @@ homekit_server_config_t config = {
     .accessories = accessories,
     .password = "111-11-111"
 };
+static bool homekit_initialized = false;
 
+void on_wifi_config_event(wifi_config_event_t event) {
+    if (event == WIFI_CONFIG_CONNECTED) {
+        printf("WIFI_CONFIG_CONNECTED\n");
+        if (!homekit_initialized) {
+            printf("Starting Homekit server...\n");
+            homekit_server_init(&config);
+            homekit_initialized = true;
+        }
+    }
+}
+void button_up_callback(button_event_t event, void* context) {
+    // up button pressed
+    printf("Button up pressed\n");
+    float newTargetTemp = target_temperature.value.float_value + 0.5;
+    target_temperature.value = HOMEKIT_FLOAT(newTargetTemp);
+    homekit_characteristic_notify(&target_temperature, target_temperature.value);
+    display_temperature(current_temperature.value.float_value, current_humidity.value.float_value);
+
+}
+
+void button_down_callback(button_event_t event, void* context) {
+    // down button pressed
+    printf("Button down pressed\n");
+    float newTargetTemp = target_temperature.value.float_value - 0.5;
+    target_temperature.value = HOMEKIT_FLOAT(newTargetTemp);
+    homekit_characteristic_notify(&target_temperature, target_temperature.value);
+    display_temperature(current_temperature.value.float_value, current_humidity.value.float_value);
+}
+void gpio_init(){
+    //gpio_set_pullup(TEMPERATURE_SENSOR_PIN, false, false);
+    gpio_enable(HEATER_PIN, GPIO_OUTPUT);
+    gpio_enable(BUTTON_DOWN_PIN, GPIO_INPUT);
+    gpio_enable(BUTTON_UP_PIN, GPIO_INPUT);
+    gpio_set_pullup(BUTTON_DOWN_PIN, true, true);
+    gpio_set_pullup(BUTTON_UP_PIN, true, true);
+    heaterOff();
+}
 void user_init(void) {
     uart_set_baud(0, 115200);
-
-    wifi_init();
+    gpio_init();
+    display_init();
+    wifi_config_init2("my-accessory", "my-password", on_wifi_config_event);
     thermostat_init();
-    homekit_server_init(&config);
+    button_config_t buttonconfig = BUTTON_CONFIG(
+        button_active_low,
+        .long_press_time = 1000,
+        .max_repeat_presses = 3,
+    );
+    if (button_create(BUTTON_UP_PIN, buttonconfig, button_up_callback, NULL)) {
+        printf("Failed to initialize button up\n");
+    }
+    if (button_create(BUTTON_DOWN_PIN, buttonconfig, button_down_callback, NULL)) {
+        printf("Failed to initialize button down\n");
+    }
 }
 
