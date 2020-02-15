bfbc357ec5c337e79458fb42f89a599fa366c5d2
Sakis <sakisdog@gmail.com>
2020-02-15 17:03:16 +0000

    Minor changes

--- examples/thermostat/thermostat.c
+++ examples/thermostat/thermostat.c
@@ -7,8 +7,9 @@
 #define TEMP_DIFF 0.5 // Set this for differential 
 #define INVERT_RELAY_SWITCH 0
 #define DHT_TYPE DHT_TYPE_DHT22 // If you are using DHT11 change the type to DHT_TYPE_DHT11
-#define BUTTON_UP_PIN 12
-#define BUTTON_DOWN_PIN 13
+#define BUTTON_UP_PIN 12 // GPIO12 is D6 on NodeMCU
+#define BUTTON_DOWN_PIN 13 // GPIO13 is D7 on NodeMCU
+#define BUTTON_RESET_PIN 14 // GPIO14 is D5 on NodeMCU
 
 #include <stdio.h>
 #include <espressif/esp_wifi.h>
@@ -78,6 +79,42 @@ static const ssd1306_t display = {
 
 static uint8_t display_buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
 
+
+void display_temperature_task(void *_args) {
+    //printf(" TEMP %g, HUM %g, TARGET: %g\n", temperature, humidity, target_temperature.value.float_value);
+    while (1) {
+        float temperature = current_temperature.value.float_value;
+        float humidity = current_humidity.value.float_value;
+        char str[16];
+        //float f = 123.456789;
+        //snprintf(str, sizeof(str), "%.2f", temperature);
+        
+        //ssd1306_display_on(&display, true);
+        
+        // Display temp
+        snprintf(str, sizeof(str), "%.1f", temperature);
+        ssd1306_fill_rectangle(&display, display_buffer, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, OLED_COLOR_BLACK);
+        ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 0, 0, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+        ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 64, 0, "C", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+        //ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 50, 0, "°", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    
+        // Display humidity
+        snprintf(str, sizeof(str), "%.1f", humidity);
+        ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 0, 24, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+        ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 64, 24, "%", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    
+        // Display target temp    
+        snprintf(str, sizeof(str), "Target: %.1f", target_temperature.value.float_value);
+        ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 0, 48, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+        ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 80, 48, "C", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+        //ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 78, 48, "°", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        if (ssd1306_load_frame_buffer(&display, display_buffer)) {
+            printf("Failed to load buffer for OLED display\n");
+        }
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
 void display_init() {
     printf("Starting display init...");
     i2c_init(I2C_BUS, I2C_SCL_PIN, I2C_SDA_PIN, I2C_FREQ_400K);
@@ -89,39 +126,9 @@ void display_init() {
     ssd1306_set_scan_direction_fwd(&display, false);
     ssd1306_set_segment_remapping_enabled(&display, true);
     ssd1306_display_on(&display, true);
+    xTaskCreate(display_temperature_task, "Display", 512, NULL, 2, NULL);
 }
 
-void display_temperature(float temperature, float humidity) {
-    //printf(" TEMP %g, HUM %g, TARGET: %g\n", temperature, humidity, target_temperature.value.float_value);
-
-    char str[16];
-    //float f = 123.456789;
-    //snprintf(str, sizeof(str), "%.2f", temperature);
-    
-    //ssd1306_display_on(&display, true);
-    
-    // Display temp
-    snprintf(str, sizeof(str), "%.1f", temperature);
-    ssd1306_fill_rectangle(&display, display_buffer, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, OLED_COLOR_BLACK);
-    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 0, 0, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 64, 0, "C", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    //ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 50, 0, "°", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-   
-    // Display humidity
-    snprintf(str, sizeof(str), "%.1f", humidity);
-    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 0, 24, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT2], 64, 24, "%", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-   
-    // Display target temp    
-    snprintf(str, sizeof(str), "Target: %.1f", target_temperature.value.float_value);
-    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 0, 48, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 80, 48, "C", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    //ssd1306_draw_string(&display, display_buffer, font_builtin_fonts[DEFAULT_FONT], 78, 48, "°", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-
-    if (ssd1306_load_frame_buffer(&display, display_buffer)) {
-        printf("Failed to load buffer for OLED display\n");
-    }
-}
 
 void thermostat_identify(homekit_value_t _value) {
     printf("Thermostat identify\n");
@@ -179,7 +186,7 @@ void temperature_sensor_task(void *_args) {
 
             homekit_characteristic_notify(&current_temperature, current_temperature.value);
             homekit_characteristic_notify(&current_humidity, current_humidity.value);
-	        display_temperature(temperature_value, humidity_value);
+	        //display_temperature(temperature_value, humidity_value);
             temp_difference = current_temperature.value.float_value - target_temperature.value.float_value;
             //printf("temp difference: %.2f\n",fabs(temp_difference));
             if (fabs(temp_difference) > TEMP_DIFF) {
@@ -195,7 +202,7 @@ void temperature_sensor_task(void *_args) {
 }
 
 void thermostat_init() {
-    xTaskCreate(temperature_sensor_task, "Thermostat", 512, NULL, 2, NULL);
+    xTaskCreate(temperature_sensor_task, "Thermostat", 256, NULL, 2, NULL);
 }
 
 homekit_accessory_t *accessories[] = {
@@ -248,7 +255,7 @@ void button_up_callback(button_event_t event, void* context) {
     float newTargetTemp = target_temperature.value.float_value + 0.5;
     target_temperature.value = HOMEKIT_FLOAT(newTargetTemp);
     homekit_characteristic_notify(&target_temperature, target_temperature.value);
-    display_temperature(current_temperature.value.float_value, current_humidity.value.float_value);
+    //display_temperature(current_temperature.value.float_value, current_humidity.value.float_value);
 
 }
 
@@ -258,7 +265,31 @@ void button_down_callback(button_event_t event, void* context) {
     float newTargetTemp = target_temperature.value.float_value - 0.5;
     target_temperature.value = HOMEKIT_FLOAT(newTargetTemp);
     homekit_characteristic_notify(&target_temperature, target_temperature.value);
-    display_temperature(current_temperature.value.float_value, current_humidity.value.float_value);
+    //display_temperature(current_temperature.value.float_value, current_humidity.value.float_value);
+}
+void button_reset_callback(button_event_t event, void* context) {
+    // down button pressed
+    switch (event) {
+        case button_event_single_press:
+            printf("single press\n");
+            break;
+        case button_event_double_press:
+            printf("double press\n");
+            break;
+        case button_event_tripple_press:
+            printf("tripple press\n");
+            break;
+        case button_event_long_press:
+            printf("long press\n");
+            printf("Resetting Wifi Config\n");
+            wifi_config_reset();
+            printf("Resetting HomeKit Config\n");
+            homekit_server_reset();
+            printf("Restarting\n");
+            sdk_system_restart();
+            break;
+    }
+    
 }
 void gpio_init(){
     //gpio_set_pullup(TEMPERATURE_SENSOR_PIN, false, false);
@@ -272,12 +303,12 @@ void gpio_init(){
 void user_init(void) {
     uart_set_baud(0, 115200);
     gpio_init();
-    display_init();
-    wifi_config_init2("my-accessory", "my-password", on_wifi_config_event);
+    wifi_config_init2("Thermostat", "mythermostat", on_wifi_config_event);
     thermostat_init();
+    display_init();
     button_config_t buttonconfig = BUTTON_CONFIG(
         button_active_low,
-        .long_press_time = 1000,
+        .long_press_time = 3000,
         .max_repeat_presses = 3,
     );
     if (button_create(BUTTON_UP_PIN, buttonconfig, button_up_callback, NULL)) {
@@ -285,6 +316,9 @@ void user_init(void) {
     }
     if (button_create(BUTTON_DOWN_PIN, buttonconfig, button_down_callback, NULL)) {
         printf("Failed to initialize button down\n");
+    }
+    if (button_create(BUTTON_RESET_PIN, buttonconfig, button_reset_callback, NULL)) {
+        printf("Failed to initialize button resetwn\n");
     }
 }
 
